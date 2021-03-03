3d68285f708343bb962017670875f8cd27ddc23d
Shoma <sho1228.k@gmail.com>
2021-03-03 21:41:19 +0900

    ちょっとした修正とダークモードの対応(未完成)

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  dark = false;
 
-  constructor() {}
+  constructor() {
+    const prefersColor = window.matchMedia('(prefers-color-scheme: dark)');
+    this.dark = prefersColor.matches;
+    this.updateDarkMode();
 
+    prefersColor.addEventListener(
+      'change',
+      mediaQuery => {
+        this.dark = mediaQuery.matches;
+        this.updateDarkMode();
+      }
+    );
+  }
+
+  updateDarkMode() {
+    console.log('hoge');
+    document.body.classList.toggle('dark', this.dark);
+  }
 }
